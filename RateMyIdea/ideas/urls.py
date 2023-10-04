093ef6efc501ecd1a7189e832ba4f42f6f221aed
Jack Pflaum <jackpflaum@gmail.com>
2023-10-04 18:20:19 +0800

    Allow media files to display during development

--- RateMyIdea/ideas/urls.py
+++ RateMyIdea/ideas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import IdeaView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'ideas'
 
@@ -17,4 +19,4 @@ urlpatterns = [
     path('author/security/<slug:slug>', views.profile_security, name='profile_security'),
     path('author/delete_account/', views.delete_account, name='delete_account'),
     path('author/delete_idea/<slug:idea_slug>/<slug:author_slug>/', views.delete_idea, name='delete_idea'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    # serves uploaded media files during development
